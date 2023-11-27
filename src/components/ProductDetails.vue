65e455f0d679e75d0244f8024e3f374c97b2428f
Roman <igoshinr.estho@gmail.com>
2023-11-27 14:33:16 +0300

    com

---
+++ src/components/ProductDetails.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>
+        <p>name: {{ product.name }}</p>
+        <p>price: {{ formattedPrice }}</p>
+        <p>{{ isAvailable }}</p>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'ProductDetails',
+
+  data () {
+    return {
+      product: {
+        name: 'ProductName',
+        price: 10,
+        available: false
+      }
+    }
+  },
+
+  computed: {
+    formattedPrice () {
+      return `$${this.product.price}`
+    },
+
+    isAvailable () {
+      if (this.product.available) {
+        return 'Available'
+      } else {
+        return 'Out of stock'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
